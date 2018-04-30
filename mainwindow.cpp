fa210c2e299604bf8536f6c56d646b65e0a2b978
javimgit1224 <36682043+javimgit1224@users.noreply.github.com>
2018-04-30 10:56:59 -0700

    Add files via upload
    added the images to be displayed to the radio buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,31 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+//this is for the first radio button
+//we want an image to open up on screen to display which R-therm user chose
+void MainWindow::on_radioButton_clicked()
+{
+    //create a pic and chose the path of the pic
+    QPixmap pix("/home/javi/Desktop/r1.jpg");
+    //use an if statement to state if the button is clicked display the pic
+    if (ui->radioButton->isChecked())
+       ui->label_4->setPixmap(pix);
+
+}
+
+//same as above
+void MainWindow::on_radioButton_2_clicked()
+{
+    QPixmap pix2("/home/javi/Desktop/r2.jpg");
+    if(ui->radioButton_2->isChecked())
+        ui->label_4->setPixmap(pix2);
 }
